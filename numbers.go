1f9b14057efb8b53b1c0a1b18d88dfe620cbb4de
Dave Appleton <calistralabs@gmail.com>
2017-10-20 01:42:35 +0800

    PointOneEther & ex OneEther

--- numbers.go
+++ numbers.go
@@ -6,6 +6,12 @@ func bi(n int) *big.Int {
 	return new(big.Int).SetUint64(uint64(n))
 }
 
-func oneEther() *big.Int {
+// OneEther as a big Int
+func OneEther() *big.Int {
 	return new(big.Int).SetUint64(1000000000000000000)
+}
+
+// PointOneEther as a big int
+func PointOneEther() *big.Int {
+	return new(big.Int).SetUint64(100000000000000000)
 }
